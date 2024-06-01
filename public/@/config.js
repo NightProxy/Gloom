e9d3f1ef9760a0932d1b77b10a06a5ae7c15c43f
Amplify <not.amplify.x@gmail.com>
2024-05-31 23:07:47 -0400

    fixed UV config

--- public/@/config.js
+++ public/@/config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/@/light/',
-    bare: '/bare/',
+    prefix: '/@/gloom/',
+    bare: 'https://lightgo.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/@/handler.js',
